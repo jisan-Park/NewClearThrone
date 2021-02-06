8b0ee36a5577fdcbb0a81892b9c7cfed048207af
DESKTOP-S83D6OK\조주연 <ssgwon1@gmail.com>
2021-02-06 15:10:24 +0900

    사운드 추가

--- playGround.cpp
+++ playGround.cpp
@@ -25,6 +25,45 @@ HRESULT playGround::init()
 	SCENEMANAGER->addScene("카드선택씬", new cardselectScene);
 
 
+	SOUNDMANAGER->addSound("메인메뉴", "sound/배경/메인메뉴.mp3", true, true);
+	SOUNDMANAGER->addSound("캐릭터선택", "sound/배경/캐릭터선택.mp3", true, true);
+	SOUNDMANAGER->addSound("스테이지1", "sound/배경/스테이지1.mp3", true, true);
+	SOUNDMANAGER->addSound("스테이지2", "sound/배경/스테이지2.mp3", true, true);
+	SOUNDMANAGER->addSound("스테이지3", "sound/배경/스테이지3.mp3", true, true);
+
+	SOUNDMANAGER->addSound("멜팅죽을떄", "sound/플레이어 사망/멜팅죽을때.mp3", false, false);
+	SOUNDMANAGER->addSound("아이즈죽을때", "sound/플레이어 사망/아이즈죽을때.mp3", false, false);
+	SOUNDMANAGER->addSound("크리스탈죽을때", "sound/플레이어 사망/크리스탈죽을때.mp3", false, false);
+	SOUNDMANAGER->addSound("플랜트죽을때", "sound/플레이어 사망/플랜트죽을대.mp3", false, false);
+	SOUNDMANAGER->addSound("피쉬죽을때", "sound/플레이어 사망/피쉬죽을때.mp3", false, false);
+
+	SOUNDMANAGER->addSound("멜팅죽을떄", "sound/아이즈 스킬.mp3", false, false);
+	SOUNDMANAGER->addSound("크리스탈죽을때", "sound/크리스탈스킬.mp3", false, false);
+	SOUNDMANAGER->addSound("플랜트죽을때", "sound/플레이어 사망/플랜트죽을대.mp3", false, false);
+	SOUNDMANAGER->addSound("피쉬죽을때", "sound/플레이어 사망/피쉬죽을때.mp3", false, false);
+
+	SOUNDMANAGER->addSound("클릭소리", "sound/효과음/클릭소리.mp3", false, false);
+	SOUNDMANAGER->addSound("폭발사운드", "sound/효과음/폭빌사운드.mp3", false, false);
+	
+	SOUNDMANAGER->addSound("멜팅 선택", "sound/효과음/캐릭터 선택/멜팅 선택.mp3", false, false);
+	SOUNDMANAGER->addSound("플랜트선택", "sound/효과음/캐릭터 선택/플랜트 선택.mp3", false, false);
+	SOUNDMANAGER->addSound("아이즈선택", "sound/효과음/캐릭터 선택/아이즈 선택.mp3", false, false);
+	SOUNDMANAGER->addSound("크리스탈 선택", "sound/효과음/캐릭터 선택/크리스탈선택.mp3", false, false);
+	SOUNDMANAGER->addSound("피쉬 선택", "sound/효과음/캐릭터 선택/피쉬 선택.mp3", false, false);
+
+	SOUNDMANAGER->addSound("까마귀 맞았을때", "sound/효과음/에너미/까마귀/까마귀 맞았을때.mp3", false, false);
+	SOUNDMANAGER->addSound("까마귀1", "sound/효과음/에너미/까마귀/까마귀1.mp3", false, false);
+	SOUNDMANAGER->addSound("까마귀2", "sound/효과음/에너미/까마귀/까마귀2.mp3", false, false);
+	
+	SOUNDMANAGER->addSound("도마뱀 불뿜기", "sound/효과음/에너미/도마뱀/불뿜기.mp3", false, true);
+	
+	SOUNDMANAGER->addSound("잡몹 죽을때1", "sound/효과음/에너미/잡몹/잡몹죽을때1.mp3", false, false);
+	SOUNDMANAGER->addSound("잡몹 죽을때2", "sound/효과음/에너미/잡몹/잡몹죽을때2.mp3", false, false);
+
+	SOUNDMANAGER->addSound("빅독도는거", "sound/효과음/에너미/빅독돌면서총쏘는.mp3", false, true);
+	SOUNDMANAGER->addSound("빅독 등장", "sound/효과음/에너미/빅독등장.mp3", false, false);
+	SOUNDMANAGER->addSound("빅밴딧 등장", "sound/효과음/에너미/빅밴딧등장1.mp3", false, false);
+	
 	SCENEMANAGER->changeScene("시작씬");
 
 	return S_OK;
